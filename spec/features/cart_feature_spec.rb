4ec2beaee414733c8a1bc44dc31be2eecbc422f5
Tina Hang <mstinahang@gmail.com>
2018-07-03 00:25:05 +0000

    automatically backed up by learn

--- spec/features/cart_feature_spec.rb
+++ spec/features/cart_feature_spec.rb
@@ -4,7 +4,7 @@ describe 'Feature Test: Cart', :type => :feature do
 
     context "logged in" do
       before(:each) do
-        @user = User.create!(email: "tina@example.com", password: "tinatina")
+        @user = User.first
         @user.current_cart = @user.carts.create
         @current_cart = @user.current_cart
         @first_item = Item.first
